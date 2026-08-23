cf06bd46e5eeb9f77553165ddb52fe02a452adec
DieuMerciMvemba <dieumercimvemba.cd@gmail.com>
2026-08-23 05:55:01 +0100

    chore: remove mqtt service and use direct http telemetry

--- src/telemetry/telemetry.module.ts
+++ src/telemetry/telemetry.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TelemetryController } from './telemetry.controller';
-import { MqttService } from './mqtt.service';
 
 @Module({
   controllers: [TelemetryController],
-  providers: [MqttService],
 })
 export class TelemetryModule {}
